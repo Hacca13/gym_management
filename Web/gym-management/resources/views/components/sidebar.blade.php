e060139e267f0113526697b68eebc601b59c2ab8
ironsiuv <s.ammirati95@gmail.com>
2019-08-27 22:26:44 +0200

    - Updating sidebar and dashboard

---
+++ Web/gym-management/resources/views/components/sidebar.blade.php
@@ -0,0 +1,54 @@
+<div class="sidebar-wrapper">
+    <div class="logo">
+        <a href="http://www.creative-tim.com" class="simple-text">
+            Fit&Fight
+        </a>
+    </div>
+
+    <ul class="nav">
+
+        <li class="active">
+            <a href="#">
+                <i class="pe-7s-home"></i>
+                <p>Home</p>
+            </a>
+        </li>
+
+        <li>
+            <a href="#">
+                <i class="pe-7s-note2"></i>
+                <p>Gestione schede</p>
+            </a>
+        </li>
+
+        <li>
+            <a href="#">
+                <i class="pe-7s-gym"></i>
+                <p>Gestione esercizi</p>
+            </a>
+        </li>
+
+        <li>
+            <a href="#">
+                <i class="pe-7s-id"></i>
+                <p>Gestione abbonamenti</p>
+            </a>
+        </li>
+
+        <li>
+            <a href="#">
+                <i class="pe-7s-users"></i>
+                <p>Gestione iscritti</p>
+            </a>
+        </li>
+
+        <li>
+            <a href="#">
+                <i class="pe-7s-notebook"></i>
+                <p>Gestione corsi</p>
+            </a>
+        </li>
+
+    </ul>
+</div>
+</div>
