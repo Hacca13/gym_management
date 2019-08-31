0bb704c3df09316f33f8e863ba5932650585fbcf
1r0n51uv <s.ammirati95@gmail.com>
2019-08-31 14:58:03 +0200

    - Fixed Home

--- Web/gym-management/resources/views/index.blade.php
+++ Web/gym-management/resources/views/index.blade.php
@@ -5,44 +5,55 @@
     <div class="row">
 
         <div class="col-md-12">
-            <div class="card">
-                <div class="card-body" style="text-align: center; padding-top: 0.2em; padding-bottom: 0.2em;">
-                    <h1>Gestione Schede</h1>
+            <div class="card card-hover">
+                <div class="box bg-cyan text-center">
+                    <h2 class="text-white" style="margin-top: auto; margin-bottom: auto; padding: 0.5em">
+                        <i class="mdi mdi-clipboard-outline"></i>
+                            Gestione Schede</h2>
                 </div>
             </div>
         </div>
 
         <div class="col-md-12">
-            <div class="card">
-                <div class="card-body" style="text-align: center; padding-top: 0.2em; padding-bottom: 0.2em;">
-                    <h1>Gestione Esercizi</h1>
+            <div class="card card-hover">
+                <div class="box bg-success text-center">
+                    <h2 class="text-white" style="margin-top: auto; margin-bottom: auto; padding: 0.5em">
+                        <i class="mdi mdi-football-helmet"></i>
+                        Gestione Esercizi</h2>
                 </div>
             </div>
         </div>
 
         <div class="col-md-12">
-            <div class="card">
-                <div class="card-body" style="text-align: center; padding-top: 0.2em; padding-bottom: 0.2em;">
-                    <h1>Gestione Abbonamenti</h1>
+            <div class="card card-hover">
+                <div class="box bg-warning text-center">
+                    <h2 class="text-white" style="margin-top: auto; margin-bottom: auto; padding: 0.5em">
+                        <i class="mdi mdi-format-list-bulleted-type"></i>
+                        Gestione Abbonamenti</h2>
                 </div>
             </div>
         </div>
 
         <div class="col-md-12">
-            <div class="card">
-                <div class="card-body" style="text-align: center; padding-top: 0.2em; padding-bottom: 0.2em;">
-                    <h1>Gestione Iscritti</h1>
+            <div class="card card-hover">
+                <div class="box bg-danger text-center">
+                    <h2 class="text-white" style="margin-top: auto; margin-bottom: auto; padding: 0.5em">
+                        <i class="mdi mdi-account-multiple"></i>
+                        Gestione Iscritti</h2>
                 </div>
             </div>
         </div>
 
         <div class="col-md-12">
-            <div class="card">
-                <div class="card-body" style="text-align: center; padding-top: 0.2em; padding-bottom: 0.2em;">
-                    <h1>Gestione Corsi</h1>
+            <div class="card card-hover">
+                <div class="box bg-info text-center">
+                    <h2 class="text-white" style="margin-top: auto; margin-bottom: auto; padding: 0.5em">
+                        <i class="mdi mdi-av-timer"></i>
+                        Gestione Corsi</h2>
                 </div>
             </div>
         </div>
+
 
     </div> 
 
