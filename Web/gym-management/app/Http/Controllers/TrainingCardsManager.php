63ee194bc1e5348d7b80637cb616a1f628c85a8c
Vincenzo <V.Noviello3@gmail.com>
2019-10-03 17:33:54 +0200

    fix project

---
+++ Web/gym-management/app/Http/Controllers/TrainingCardsManager.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Models\TrainingCardsModel;
+
+class TrainingCardsManager extends Controller
+{
+
+    public static function transformTrainingCardsIntoArrayTrainingCards($trainingCards){
+
+      $idDatabase = $trainingCards->getIdDatabase();
+      $idUserDatabase = $trainingCards->getIdUserDatabase();
+      $period = $trainingCards->getPeriod();
+      $exercises = $trainingCards->getExercises();
+
+      $arrayTrainingCards = array(
+        'idDatabase' => $idDatabase,
+        'idUserDatabase' => $idUserDatabase,
+        'period' => $period,
+        'exercises' => $exercises
+      );
+
+      return $arrayTrainingCards;
+    }
+    public static function transformArrayTrainingCardsIntoTrainingCards($arrayTrainingCards){
+      $idDatabase = data_get($arrayTrainingCards,'idDatabase');
+      $idUserDatabase = data_get($arrayTrainingCards,'idUserDatabase');
+      $period = data_get($arrayTrainingCards,'period');
+      $exercises = data_get($arrayTrainingCards,'exercises');
+
+      $trainingCards = new TrainingCardsModel($idDatabase,$idUserDatabase,$period,$exercises);
+
+      return $trainingCards;
+    }
+}
