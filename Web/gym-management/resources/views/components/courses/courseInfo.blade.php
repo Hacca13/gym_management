d50155919742e8322710cfe9c04dc508234f5a98
mirko <mirkoali24@gmail.com>
2019-10-14 11:33:02 +0200

    CourseInfo

---
+++ Web/gym-management/resources/views/components/courses/courseInfo.blade.php
@@ -0,0 +1,58 @@
+
+<div class="col-md-8">
+
+    <div class="card" style="border-radius: 10px">
+
+        <div class="card-header">
+
+            <div class="row">
+
+                <div class="col-md-6" style="text-align: start">
+                    <a href="#">
+                        <i class="fas fa-edit" style="font-size: 150%; color: black;"></i>
+                    </a>
+
+                </div>
+
+                <div class="col-md-6" style="text-align: end">
+
+                    <a href="#">
+                        <i class="fas fa-times" style="font-size: 170%; color: red; margin-left: 2.5%"></i>
+                    </a>
+
+                </div>
+
+            </div>
+
+        </div>
+        <form class="form-horizontal" method="get">
+        <div class="card-body">
+            <div class="form-group row">
+                <label for="fname" class="col-sm-3 text-right control-label col-form-label">Nome Corso</label>
+                <div class="col-sm-9">
+                    <input type="" class="form-control" id="fname" name="name">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="fname" class="col-sm-3 text-right control-label col-form-label">Nome Istruttore</label>
+                <div class="col-sm-9">
+                    <input type="text" class="form-control" id="fname" name="name">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="fname" class="col-sm-3 text-right control-label col-form-label">Durata del Corso</label>
+                <div class="col-sm-9">
+                    <input type="text" class="form-control" id="fname" name="name">
+                </div>
+            </div>
+            <div class="form-group row">
+                <label for="fname" class="col-sm-3 text-right control-label col-form-label">Immagine del Corso</label>
+                <div class="col-sm-9">
+                    <input type="file" class="form-control" id="fname" name="name">
+                </div>
+            </div>
+        </div>
+        </form>
+    </div>
+
+</div>
