0d542516e32697e747395919c8b92032fa89770d
Memo <p.szopian@studenti.unisa.it>
2019-10-15 10:32:18 +0200

    Search bar viewFix

--- Web/gym-management/resources/views/courses.blade.php
+++ Web/gym-management/resources/views/courses.blade.php
@@ -2,7 +2,6 @@
 
 @section('content')
 
-
     <div class="row">
 
         <div class="col-md-12" style="text-align: center;">
@@ -10,47 +9,45 @@
         </div>
 
         <div class="col-md-12" style="margin-top: 2.5%">
+            <div class="card">
 
-            <div class="row">
+                <div class="card-body">
+                    <div class="row">
 
-                <div class="col-md-10">
+                        <div class="col-md-10">
 
-                    <div class="input-group no-border">
-                        <input type="text" value="" class="form-control" placeholder="Search...">
-                        <button type="submit" class="btn btn-default btn-round btn-just-icon">
-                            <i class="fas fa-search"></i>
-                            <div class="ripple-container"></div>
-                        </button>
+                            <div class="input-group no-border">
+                                <input type="text" value="" class="form-control" placeholder="Search...">
+                                <button type="submit" class="btn btn-default btn-round btn-just-icon">
+                                    <i class="fas fa-search"></i>
+                                    <div class="ripple-container"></div>
+                                </button>
+                            </div>
+                        </div>
+
+                        <div class="col-md-2" style="text-align: center; margin-top: auto; margin-bottom: auto">
+                            <a href="insertCourse" role="button">
+                                <button class="bttn-material-circle bttn-md bttn-success">
+                                    <i class="fas fa-plus"></i>
+                                </button>
+                            </a>
+                        </div>
+
                     </div>
-
-
                 </div>
-
-                <div class="col-md-2" style="text-align: center; margin-top: auto; margin-bottom: auto">
-
-                    <a href="insertCourse" role="button">
-                        <button class="bttn-material-circle bttn-md bttn-success">
-                            <i class="fas fa-plus"></i>
-                        </button>
-                    </a>
-
-                </div>
-
             </div>
+        </div>
+    </div>
 
+    @foreach($courses as $course)
+        <div class="col-md-12">
 
+            @include('components.courses.courseCard')
 
         </div>
-
-        @foreach($courses as $course)
-            <div class="col-md-12">
-
-                @include('components.courses.courseCard')
-
-            </div>
         @endforeach
 
-    </div>
+        </div>
 
 
 @endsection
