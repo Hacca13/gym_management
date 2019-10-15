cdf2074e5497676aa91b6e7abe95bfe054185321
Memo <p.szopian@studenti.unisa.it>
2019-10-15 13:56:59 +0200

    Deleted the navbar

--- Web/gym-management/resources/views/components/sidebar.blade.php
+++ Web/gym-management/resources/views/components/sidebar.blade.php
@@ -1,15 +1,12 @@
 <aside class="left-sidebar" data-sidebarbg="skin5">
-    <div class="scroll-sidebar">
-        <nav class="sidebar-nav">
-            <ul id="sidebarnav" class="p-t-30">
-                <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="/" aria-expanded="false"><i class="mdi mdi-home"></i><span class="hide-menu">Home</span></a></li>
-                <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="/schede" aria-expanded="false"><i class="fas fa-clipboard-list"></i><span class="hide-menu">Gestione Schede</span></a></li>
-                <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="/esercizi" aria-expanded="false"><i class="fab fa-algolia"></i><span class="hide-menu">Gestione Esercizi</span></a></li>
-                <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="/abbonamenti" aria-expanded="false"><i class="fas fa-euro-sign"></i><span class="hide-menu">Gestione Abbonamenti</span></a></li>
-                <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="/iscritti" aria-expanded="false"><i class="fas fa-users"></i><span class="hide-menu">Gestione Iscritti</span></a></li>
-                <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="/corsi" aria-expanded="false"><i class="mdi mdi-av-timer"></i><span class="hide-menu">Gestione Corsi</span></a></li>
-
-            </ul>
-        </nav>
-    </div>
+    <nav class="sidebar-nav">
+        <ul id="sidebar-nav">
+            <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="/" aria-expanded="false"><i class="mdi mdi-home"></i><span class="hide-menu">Fit & Fight</span></a></li>
+            <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="/schede" aria-expanded="false"><i class="fas fa-clipboard-list"></i><span class="hide-menu">Gestione Schede</span></a></li>
+            <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="/esercizi" aria-expanded="false"><i class="fab fa-algolia"></i><span class="hide-menu">Gestione Esercizi</span></a></li>
+            <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="/abbonamenti" aria-expanded="false"><i class="fas fa-euro-sign"></i><span class="hide-menu">Gestione Abbonamenti</span></a></li>
+            <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="/iscritti" aria-expanded="false"><i class="fas fa-users"></i><span class="hide-menu">Gestione Iscritti</span></a></li>
+            <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="/corsi" aria-expanded="false"><i class="mdi mdi-av-timer"></i><span class="hide-menu">Gestione Corsi</span></a></li>
+        </ul>
+    </nav>
 </aside>
