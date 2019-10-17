eb17ed695a77adfc1865df6ce6bb768fede1ce3a
Memo <p.szopian@studenti.unisa.it>
2019-10-17 20:28:42 +0200

    Fix insertNewExercise and changeEx.

--- Web/gym-management/resources/views/changeEx.blade.php
+++ Web/gym-management/resources/views/changeEx.blade.php
@@ -5,13 +5,14 @@
             <div class="card" style="border-radius: 10px;background-color: rgb(255, 255, 255,0.7);">
                 <div class="card-header">
                     <div class="row">
-                        <div class="col-md-8" style="text-align: left">
-                            <h4>Modifica esercizio</h4>
+                        <div class="col-md-1"></div>
+                        <div class="col-md-10" style="text-align: center">
+                            <h2>Modifica esercizio</h2>
                         </div>
-                        <div class="col-md-4" style="text-align: end">
+                        <div class="col-md-1" style="text-align: end">
                             <!--Pagina precedente-->
                             <a href="/gestioneEsercizi">
-                                <button class="btn btn-danger" id="fname" name="" style="border-radius: 10px;">Annulla</button>
+                                <i class="fas fa-times" style="font-size: 170%; color: red; margin-left: 2.5%"></i>
                             </a>
                         </div>
                     </div>
@@ -21,32 +22,36 @@
                         @csrf
                         <div class="form-group row">
                             <div class="col-sm-6">
-                                <label for="fname" class="col-sm-12 text-left control-label col-form-label">Nome Esercizio</label>
+                                <label for="fname" class="col-sm-12 text-left control-label col-form-label" style="font-size: large">Nome Esercizio</label>
                                 <input type="text" class="form-control" placeholder="#" id="fname" name="name" style="border-radius: 10px;background-color: rgb(255, 255, 255,0.7);">
                             </div>
-                            <div class="col-md-6">
-
-                            </div>
+                            <div class="col-md-6"></div>
                         </div>
                         <div class="form-group row">
                             <div class="col-sm-6">
-                                <label for="fname" class="col-sm-12 text-left control-label col-form-label">Descrizione</label>
+                                <label for="fname" class="col-sm-12 text-left control-label col-form-label" style="font-size: large">Descrizione</label>
                                 <textarea  class="form-control" placeholder="#" id="fname" name="description" rows="5" style="border-radius: 10px;background-color: rgb(255, 255, 255,0.7);"></textarea>
                             </div>
                             <div class="col-md-6">
-                                <label for="fname" class="col-sm-12 text-left control-label col-form-label">Carica Gif</label>
-                                <input type="file" class="form-control" id="fname" name="image" style="border-radius: 10px;background-color: rgb(255, 255, 255,0.7);">
+                                <label for="fname" class="col-sm-12 text-left control-label col-form-label" style="font-size: large">Carica Gif</label>
+                                <input type="file" class="form-control" id="fname" name="image" style="border-radius: 10px;background-color: rgb(255, 255, 255,0.7); size: A5">
                             </div>
                         </div>
                         <div class="form-group row">
-                            <div class="col-sm-6">
-                                <label for="fname" class="col-sm-12 text-left control-label col-form-label">Link esercizio</label>
+                            <div class="col-sm-12">
+                                <label for="fname" class="col-sm-12 text-left control-label col-form-label" style="font-size: large">Link esercizio</label>
                                 <input class="form-control" placeholder="#" id="fname" name="link" style="border-radius: 10px; background-color: rgb(255, 255, 255,0.7);">
                             </div>
                         </div>
                         <div class="form-group row">
-                            <div class="col-sm-9">
+                            <div class="col-md-8"></div>
+                            <div class="col-sm-2">
                                 <button class="btn btn-success" id="fname" name="" style="border-radius: 10px;">Inserisci</button>
+                            </div>
+                            <div class="col-md-2">
+                                <a href="/gestioneEsercizi">
+                                    <button class="btn btn-danger" id="fname" name="" style="border-radius: 10px;">Annulla</button>
+                                </a>
                             </div>
                         </div>
                     </form>
--- Web/gym-management/resources/views/insertNewExcercise.blade.php
+++ Web/gym-management/resources/views/insertNewExcercise.blade.php
@@ -7,12 +7,12 @@
                 <div class="card-header">
                     <div class="row">
                         <div class="col-md-8" style="text-align: left">
-                            <h4>Inserisci Eserizio</h4>
+                            <h2>Inserisci Eserizio</h2>
                         </div>
                         <div class="col-md-4" style="text-align: end">
                             <!--Pagina precedente-->
                             <a href="/gestioneEsercizi">
-                                <button class="btn btn-danger" id="fname" name="" style="border-radius: 10px;">Annulla</button>
+                                <i class="fas fa-times" style="font-size: 170%; color: red; margin-left: 2.5%"></i>
                             </a>
                         </div>
                     </div>
@@ -22,7 +22,7 @@
                     @csrf
                         <div class="form-group row">
                             <div class="col-sm-6">
-                                <label for="fname" class="col-sm-12 text-left control-label col-form-label">Nome Esercizio</label>
+                                <label for="fname" class="col-sm-12 text-left control-label col-form-label" style="font-size: large">Nome Esercizio</label>
                                 <input type="text" class="form-control" id="fname" name="name" style="border-radius: 10px;background-color: rgb(255, 255, 255,0.7);">
                             </div>
                             <div class="col-md-6">
@@ -31,24 +31,30 @@
                         </div>
                         <div class="form-group row">
                             <div class="col-sm-6">
-                                <label for="fname" class="col-sm-12 text-left control-label col-form-label">Descrizione</label>
+                                <label for="fname" class="col-sm-12 text-left control-label col-form-label" style="font-size: large">Descrizione</label>
                                 <textarea  class="form-control" id="fname" name="description" rows="5" style="border-radius: 10px;background-color: rgb(255, 255, 255,0.7);"></textarea>
                             </div>
                             <div class="col-md-6">
-                                <label for="fname" class="col-sm-12 text-left control-label col-form-label">Carica Gif</label>
+                                <label for="fname" class="col-sm-12 text-left control-label col-form-label" style="font-size: large">Carica Gif</label>
                                 <input type="file" class="form-control" id="fname" name="image" style="border-radius: 10px;background-color: rgb(255, 255, 255,0.7);">
                             </div>
 
                         </div>
                         <div class="form-group row">
                             <div class="col-sm-6">
-                                <label for="fname" class="col-sm-12 text-left control-label col-form-label">Link esercizio</label>
+                                <label for="fname" class="col-sm-12 text-left control-label col-form-label" style="font-size: large">Link esercizio</label>
                                 <input class="form-control" id="fname" name="link" style="border-radius: 10px; background-color: rgb(255, 255, 255,0.7);">
                             </div>
                         </div>
                         <div class="form-group row">
-                            <div class="col-sm-9">
+                            <div class="col-md-8"></div>
+                            <div class="col-sm-2">
                                 <button class="btn btn-success" id="fname" name="" style="border-radius: 10px;">Inserisci</button>
+                            </div>
+                            <div class="col-md-2">
+                                <a href="/gestioneEsercizi">
+                                    <button class="btn btn-danger" id="fname" name="" style="border-radius: 10px;">Annulla</button>
+                                </a>
                             </div>
                         </div>
                     </form>
