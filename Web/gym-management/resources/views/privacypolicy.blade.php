8ca059c923007ff962f4fce6c3719e622f375691
mirko <mirkoali24@gmail.com>
2019-11-29 14:47:19 +0100

    Privacy Policy

---
+++ Web/gym-management/resources/views/privacypolicy.blade.php
@@ -0,0 +1,89 @@
+@extends('layouts.master')
+
+@section('content')
+
+
+    <div class="card" style="border-radius: 10px; background: #d6d8d8">
+        <div class="card-body">
+
+
+            <h3 class="row justify-content-center">PRIVACY POLICY</h3>
+            <br>
+            <br>
+
+
+            <h4>INFORMATIVA AL TRATTAMENTO DEI DATI PERSONALI</h4>
+
+            <p>I dati personali dell’utente sono utilizzati da Fit & Fight s.a.s. Di Stefano D'Alessandro & C. , che ne è titolare per il trattamento, nel rispetto dei principi di protezione dei dati personali stabiliti dal Regolamento GDPR 2016/679 e della normativa nazionale in vigore.</p>
+
+            <h4>MODALITÀ E FINALITÀ DEL TRATTAMENTO DATI</h4>
+
+            <p> La informiamo che i dati verranno trattati con il supporto dei seguenti mezzi:</p>
+            <p>	• Informatica</p>
+            <p>con le seguenti finalità:</p>
+
+            <p> • Gestire specifiche richieste inoltrate all’azienda tramite Sito Web</p>
+
+            <h4>BASE GIURIDICA</h4>
+
+            <p>La base giuridica su cui si fonda il trattamento per i dati comuni, secondo l’Art.6 del Regolamento GDPR, è:</p>
+
+            <p>La società tratta i dati facoltativi degli utenti in base al consenso, ossia mediante l’approvazione esplicita della presente policy privacy e in relazione alle modalità e finalità di seguito descritte.<p>
+
+            <h4>CATEGORIE DI DESTINATARI</h4>
+
+            <p>Ferme restando le comunicazioni eseguite in adempimento di obblighi di legge e contrattuali, tutti i dati raccolti ed elaborati potranno essere comunicati esclusivamente per le finalità sopra specificate alle seguenti categorie di destinatari:</p>
+            <p>• Contitolari;</p>
+            <p>• Titolare del trattamento;</p>
+            <p>• Firebase.</p>
+
+        <p>Nella gestione dei suoi dati, inoltre, possono venire a conoscenza degli stessi le seguenti categorie di persone autorizzate e/o responsabili interni ed esterni individuati per iscritto ed ai quali sono state fornite specifiche istruzioni scritte circa il trattamento dei dati</p>
+
+        <h4>PERIODO DI CONSERVAZIONE</h4>
+
+           <p>Il periodo di conservazione dei dati è: I dati vengono mantenuti fino all’adempimento delle finalità per cui sono stati raccolti, in seguito vengono conservati in un Database di Firebase Storage per un eventuale necessità di riutilizzo dei dati stessi.</p>
+
+            <h4>DIRITTI DELL’INTERESSATO</h4>
+
+           <p>Ai sensi del Regolamento europeo 679/2016 (GDPR) e della normativa nazionale in vigore, l’interessato può, secondo le modalità e nei limiti previsti dalla vigente normativa, esercitare i seguenti diritti:</p>
+            <p>• Richiedere la conferma dell’esistenza di dati personali che lo riguardano (diritto di accesso dell’interessato – art. 15 del Regolamento 679/2016);</p>
+            <p>• Conoscerne l’origine;</p>
+            <p>• Riceverne comunicazione intelligibile;</p>
+            <p>• Avere informazioni circa la logica, le modalità e le finalità del trattamento;</p>
+            <p>• Richiederne l’aggiornamento, la rettifica, l’integrazione, la cancellazione, la trasformazione in forma anonima, il blocco dei dati trattati in violazione di legge, ivi compresi quelli non più necessari al perseguimento degli scopi per i quali sono stati raccolti (diritto di rettifica e cancellazione – artt. 16 e 17 del Regolamento 679/2016);</p>
+            <p>• Diritto di limitazione e/o di opposizione al trattamento dei dati che lo riguardano (art. 18 del Regolamento 679/2016);</p>
+            <p>• Diritto di revoca;</p>
+            <p>• Diritto alla portabilità dei dati (art. 20 del Regolamento 679/2016);</p>
+            <p>• Nei casi di trattamento basato su consenso, ricevere i propri dati forniti al titolare, in forma strutturata e leggibile da un elaboratore di dati e in un formato comunemente usato da un dispositivo elettronico;</p>
+            <p>• Il diritto di presentare un reclamo all’Autorità di controllo (diritto di accesso dell’interessato – art. 15 del Regolamento 679/2016).</p>
+
+            <p>Titolare del trattamento dei Suoi dati personali è Fit & Fight s.a.s. Di Stefano D'Alessandro & C., p.iva 05737660653</p>
+
+            <br>
+            <br>
+            <p>Email: palestra.fit.fight@gmail.com</p>
+            <br>
+            <br>
+
+            <p>PEC: dalessandro.stefano@pecaruba.it</p>
+            <br>
+            <br>
+
+            <br>
+            <br>
+            <p>Telefono: 3272911356</p>
+
+            <p>Contitolari del trattamento dei Suoi dati personali sono:</p>
+            <p>STEFANO D'ALESSANDRO</p>
+            <p>ELISABETTA PUGLIA</p>
+
+        </div>
+    </div>
+
+
+
+
+
+
+
+@endsection
